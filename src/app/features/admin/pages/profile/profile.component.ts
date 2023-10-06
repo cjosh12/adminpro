22cc6d4665971e8c87148aaa2091b144275660e8
cjosh12 <carloscalderon826@gmail.com>
2023-10-05 19:08:40 -0600

    starting the user profile

---
+++ src/app/features/admin/pages/profile/profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    templateUrl: './profile.component.html',
+    styles: [
+
+    ]
+})
+export class ProfileComponent {
+    public title = 'Perfil de Usuario';
+    public topMargin = 'mt-2';
+}
---
+++ src/app/shared/components/title-card/title-card.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from "@angular/common";
+import { Component, Input } from "@angular/core";
+
+@Component({
+    selector: 'shared-title-card',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './title-card.component.html'
+})
+export class TitleCardComponent{
+
+    @Input()
+    title!: string;
+
+    @Input()
+    topMargin: string = 'mt-6';
+
+    
+}
